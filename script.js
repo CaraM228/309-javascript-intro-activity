d6351a0dda5c6297d948eea33d89deb0d8ab4ebf
CaraM228 <cara.macinnis@edu.sait.ca>
2026-09-17 20:47:58 -0600

    added steps 1 and 2

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
 "use strict";
 
 /* ACTIVITY 1: Query Selector and rendering strings */
+
 // create a string that cannot be changed (hint: const vs let)
+const pageTitle = "My Favourite Characters";
+
 // create a variable for the querySelector/getElementById for id="page-title"
+const title = document.getElementById("page-title");
+
 // pick between innerHTML, innerText, or textContent to render the string (hint: look up which one is most secure and performant)
+title.textContent = pageTitle;
 
 /* Activity 2: Render an array into DOM Nodes */
+
 // create an array with 5 of your favourite characters and shows
+const characters = [
+    "Spongebob",
+    "Patrick",
+    "Squidward",
+    "Sandy",
+    "Plankton",
+];
+console.log(characters);
+
 // log them manually to the DOM like arr[0]
+characters.forEach(characters=>{console.log(characters)});
+
 // create a query selector for favourite-characters
+const characterList = document.getElementById("favourite-characters");
+
 // write a loop that adds all <li>list item</li> to the ul via your querySelector (hint: + vs +=)
+characters.forEach((character) => {
+    const li = document.createElement("li");
+    li.textContent = character;
+    characterList.appendChild(li);
+});
+
 
 /* Activity 2: Rendering values from objects */
 // create an object for a user that has a name and email k/v pairs (use const)
